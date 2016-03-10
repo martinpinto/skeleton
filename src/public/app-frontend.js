20683516d6f7376b882d9f6f0316b0ab6e07e100
Martin Pinto-Bazurco <martinpintob@gmail.com>
2016-03-10 15:57:02 +0100

    Added sample weather API call code

--- src/public/app-frontend.js
+++ src/public/app-frontend.js
@@ -12,6 +12,15 @@ weatherApp.config(function($routeProvider) {
             templateUrl: 'html/forecast.html',
             controller: 'forecastController'
         })
+        .when('/forecast/:days', {
+            templateUrl: 'html/forecast.html',
+            controller: 'forecastController'
+        })
+});
+
+// SERVICES
+weatherApp.service('cityService', function() {
+    this.city = 'New York, NY';
 });
 
 // CONTROLLERS
@@ -24,11 +33,17 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
     
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', 'cityService', '$resource', '$routeParams', function($scope, cityService, $resource, $routeParams) {
     $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || 2; // default to 2 
+    
+    var appid = '829cd2f80080d0cc54367583001a15dd';
+    $scope.weatherApi = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?appid=' + appid, { callback: 'JSON_CALLBACK' }, { get: { method: 'JSONP' }});
+    
+    $scope.weatherResult = $scope.weatherApi.get({ q: $scope.city, cnt: $scope.days, units: 'metric'});
+    
+    $scope.convertToDate = function convertToDate(dt) {
+        return new Date(dt * 1000);
+    }
 }]);
-
-// SERVICES
-weatherApp.service('cityService', function() {
-    this.city = 'New York, NY';
-});
